bdc4d7bf57c28cf7c539351c416ae1b7e8205425
erazinkov <egorrazinkov@gmail.com>
2026-09-15 12:18:25 +0300

    small fix

--- energypeakfinder.cpp
+++ energypeakfinder.cpp
@@ -45,9 +45,9 @@ void EnergyPeakFinder::process(TH1D *hist, TH1D *histRc)
     std::vector<EnergyPeak> peaks{
                 EnergyPeak{EnergyPeak::Id::FE847, 0.0},
                 EnergyPeak{EnergyPeak::Id::FE1238, 0.0},
-//                 EnergyPeak{EnergyPeak::Id::HYDROGEN, 0.0},
-                EnergyPeak{EnergyPeak::Id::CARBON, 0.0},
-//                EnergyPeak{EnergyPeak::Id::SILICON, 0.0},
+                 EnergyPeak{EnergyPeak::Id::HYDROGEN, 0.0},
+//                EnergyPeak{EnergyPeak::Id::CARBON, 0.0},
+                EnergyPeak{EnergyPeak::Id::SILICON, 0.0},
                 EnergyPeak{EnergyPeak::Id::OXYGEN, 0.0},
                 EnergyPeak{EnergyPeak::Id::FE7631, 0.0},
     };
